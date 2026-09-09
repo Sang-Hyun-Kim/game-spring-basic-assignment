2550bbf57f2d7bfe35a20ddd20d7d002f157153c
Sang-Hyun-Kim <960322ksh@hanmail.net>
2026-09-09 14:26:15 +0900

    lvl 4,5 해결 로그 -lvl 4 해결 과정 --새게임 에러 이름 :Connection is read-only Queries leading to data modification are not allowed --연결관리 GameService 에서 @Transactional 어노테이션 인자값이 readOnly = false 임을 확인 - lvl 5 해결 과정 -- RunCardRequest=> CreateRequest.java의 @Valide 어노테이션을 통해 검증이 실행되고 검증에 대한 문제로 API 명세에 맞춰 Card의 응답,요청dto를 작성하는 문제 - 5-1 RunCardRequest  - 기존 ProgressRequest .java dto 코드를 참고 어노테이션을 설정  - 여기서 NotEmpty가 아닌 NotBlank를 설정, 기존 NotEmpty는 공백을 받을 수 있다는 Claude의 사후 피드백을 받아수정함, 필수 + 공백 불허를 동시에 가진다.  - NotNull + Min +Max => acquiredFloor의 어노테이션 다만 여기서 int가 아닌 Integer라는 피드백이 있엇는데 '래퍼 타입 Integer여야 @NotNull이 의미를 가진다(int는 null이 될 수 없어 0으로 들어온다)'라고 설명함 - 5-2 CardResponse - 주요 코드는 GameDetailResponse를 참고함 - 모든 멤버 한정자 및 final 연산자 추가 - Jackson이 getter가 있는 DTO 필드명을 통해 JSON키를 생성

--- src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
+++ src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
@@ -1,10 +1,19 @@
 package com.gamebasic.runcard.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class RunCardRequest {
-    // TODO (Lv 5): API 명세의 카드 필드 제약을 Bean Validation 어노테이션으로 붙이세요.
+    // NotEmpty, NotBlank 중에 공백 허용 가능성에 대해 피드백 받고 @NotBlank를 고름
+    @NotBlank
     private String cardType;
+
+    @NotNull
+    @Min(0)
+    @Max(10)
     private Integer acquiredFloor;
 }
